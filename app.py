f1c67e98ff036dc57d67a7352be18828c4dd137f
Azhaan <usazhaan@icloud.com>
2022-01-30 17:58:51 -0600

    basic UI

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/app", methods=["GET", "POST"])
+def application():
+    if request.method == "GET":
+        return render_template("layout.html")
+    else:
+        print()
